b680be9e4b9247ae2b52db096d3756a23a30b230
Guilherme Migliano <guilherme@MacBook-Air-de-Guilherme.local>
2026-01-07 17:22:27 -0300

    novas correções.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,8 +16,8 @@ import {
 
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-//import { createWeeklyMatchByAdmin } from '@/lib/services/createWeeklyMatchByAdmin';
-import { createWeeklyMatch } from '@/lib/services/createWeeklyMatch';
+import { createWeeklyMatchByAdmin } from '@/lib/services/createWeeklyMatchByAdmin';
+
 import { toast } from 'sonner';
 import { useTransition } from 'react';
 
@@ -44,7 +44,7 @@ export function Header({ user }: HeaderProps) {
   async function createMatch() {
     startTransition(async () => {
       try {
-        const res = await createWeeklyMatch();
+        const res = await createWeeklyMatchByAdmin();
         if (res.error) {
           toast.warning('Partida da semana já existente');
         } else {
