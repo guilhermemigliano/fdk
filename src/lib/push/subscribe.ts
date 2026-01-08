0ed34f2dc672f1c335532f27f26b0ed9c4505b6a
Guilherme Migliano <guilherme@MacBook-Air-de-Guilherme.local>
2026-01-08 19:06:18 -0300

    ajustes finos.

--- src/lib/push/subscribe.ts
+++ src/lib/push/subscribe.ts
@@ -1,12 +1,16 @@
 export async function subscribePush() {
   if (typeof window === 'undefined') return;
 
+  console.log('entrou aqui 1');
+
   if (!('Notification' in window)) {
+    console.log('entrou aqui 2');
     return 'unsupported';
   }
 
   // Se já está permitido
   if (Notification.permission === 'granted') {
+    console.log('entrou aqui 3');
     await doSubscribe();
     return 'granted';
   }
@@ -15,6 +19,7 @@ export async function subscribePush() {
   const permission = await Notification.requestPermission();
 
   if (permission !== 'granted') {
+    console.log('entrou aqui 4');
     return permission; // "denied"
   }
 
@@ -23,15 +28,24 @@ export async function subscribePush() {
 }
 
 async function doSubscribe() {
+  console.log('entrou aqui 5');
   const registration = await navigator.serviceWorker.ready;
+
+  console.log('entrou aqui 6');
+  console.log(registration);
 
   const subscription = await registration.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
   });
 
+  console.log('entrou aqui 7');
+  console.log(subscription);
+
   await fetch('/api/push/subscribe', {
     method: 'POST',
     body: JSON.stringify(subscription),
   });
+
+  return;
 }
